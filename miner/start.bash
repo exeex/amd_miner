d0ca9e1729632b0ba21fde2825007ea3fba84739
exeex <cswu@gapp.nthu.edu.tw>
2018-01-14 15:28:36 +0800

    Update start.bash

--- miner/start.bash
+++ miner/start.bash
@@ -1,1 +1,1 @@
-./ethdcrminer64 -epool eth.gpumine.org:3333 -ewal 0xe1C098F8fB47EbA1a8274aA1e0638D5C6f4831aE -eworker hello
+./ethdcrminer64 -epool eth.gpumine.org:3333 -ewal 0xe1C098F8fB47EbA1a8274aA1e0638D5C6f4831aE -eworker hello -wd 0 -r 1
